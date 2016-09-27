bcbe2144a54efbe256a9406b7c3eacff4d1b44c2
kajyr <carlo.panzi@gmail.com>
2016-09-27 18:12:18 +0200

    test doppia coppia e full

--- index.js
+++ index.js
@@ -3,37 +3,34 @@
 const _ = require("lodash")
 
 const groupByRank = (cards) => _.values(_.groupBy(cards, 'rank'))
+const groupsOf = (grouped, qty) => grouped.filter((rank) => rank.length === qty)
+const coppie = (grouped) => groupsOf(grouped, 2)
+const tris = (grouped) => groupsOf(grouped, 3)
 
 module.exports = {
 
 	hasCoppia: (cards) => {
 		let grouped = groupByRank(cards)
-		let coppie = grouped.filter((rank) => rank.length === 2)
-		return coppie.length > 0
+		return coppie(grouped).length > 0
 	},
 	
 	hasTris: (cards) => {
 		let grouped = groupByRank(cards)
-		let tris = grouped.filter((rank) => rank.length === 3)
-		return tris.length > 0
+		return tris(grouped).length > 0
 	},
 
 	hasPoker: (cards) => {
 		let grouped = groupByRank(cards)
-		let tris = grouped.filter((rank) => rank.length === 4)
-		return tris.length > 0
+		return groupsOf(grouped, 4).length > 0
 	},
 
 	hasDoppiaCoppia: (cards) => {
 		let grouped = groupByRank(cards)
-		console.log(grouped)
-		let tris = grouped.filter((rank) => rank.length === 4)
-		return tris.length > 0
+		return coppie(grouped).length === 2
 	},
 
 	hasFull: (cards) => {
 		let grouped = groupByRank(cards)
-		let tris = grouped.filter((rank) => rank.length === 4)
-		return tris.length > 0
+		return tris(grouped).length === 1 && coppie(grouped).length === 1
 	}
 }
--- tests/index.js
+++ tests/index.js
@@ -11,10 +11,9 @@ test('Test coppia', function (t) {
 	let hand = [ {rank: 'J', type: 'C'}, {rank: 'K', type: 'C'}]
 
 	let cards = table.concat(hand)
-	let cardsWithoutPair = table.concat([ {rank: 'J', type: 'C'}, {rank: '7', type: 'C'}])
 
 	t.ok(detector.hasCoppia(cards), 'Viene trovata la coppia')
-	t.notOk(detector.hasCoppia(cardsWithoutPair), 'Non viene trovata la coppia quando non c\'è')
+	t.notOk(detector.hasCoppia(table), 'Non viene trovata la coppia quando non c\'è')
 
 	t.end()
 })
@@ -52,11 +51,10 @@ test('Test doppia coppia', function (t) {
 		]
 	let hand = [ {rank: '3', type: 'C'}, {rank: 'Q', type: 'Q'}]
 
-	let cards = table2.concat(hand)
-	let cardsWithouDoppiaCoppia = table.concat([ {rank: 'J', type: 'C'}, {rank: '7', type: 'C'}])
+	let cards = table.concat(hand)
 
-	t.ok(detector.hasFull(cards), 'Viene trovato il doppia coppia')
-	t.notOk(detector.hasFull(cardsWithouDoppiaCoppia), 'Non viene trovato il doppia coppia quando non c\'è')
+	t.ok(detector.hasDoppiaCoppia(cards), 'Viene trovato il doppia coppia')
+	t.notOk(detector.hasDoppiaCoppia(table), 'Non viene trovato il doppia coppia quando non c\'è')
 	
 	t.end()
 })
@@ -69,7 +67,7 @@ test('Test full', function (t) {
 		]
 	let hand = [ {rank: '3', type: 'C'}, {rank: '2', type: 'Q'}]
 
-	let cards = table2.concat(hand)
+	let cards = table.concat(hand)
 	let cardsWithoutFull = table.concat([ {rank: 'J', type: 'C'}, {rank: '7', type: 'C'}])
 
 	t.ok(detector.hasFull(cards), 'Viene trovato il full')
